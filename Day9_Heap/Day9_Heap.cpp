79182237f4ea9251919065fa38c1efa76db6b2b2
ChenXin <xxwd001@gmail.com>
2014-12-31 16:32:01 +0800

    update main.cpp

--- Day9_Heap/Day9_Heap.cpp
+++ Day9_Heap/Day9_Heap.cpp
@@ -6,25 +6,36 @@
 #include <string.h>
 
 
-
+#pragma pack(1)
 //int b=20;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char *p1="name";
-	char *p2;
-	p2=(char*)malloc(20);
-	memset(p2,0,20);
-	while(*p2++=*p1++){
-		printf("%s",p2-1);
-	}
-	printf("\n");
-	//static int a=10;
+	//char *p1="name";
+	//char *p2;
+	//p2=(char*)malloc(20);
+	//memset(p2,0,20);
+	//while(*p2++=*p1++){
+	//	printf("%s",p2-1);
+	//}
+	//printf("\n");
+	////static int a=10;
 
-	char *p="Hello World";
-	*p++;
-	printf("%s\n",p);
-	p++;
-	printf("%s\n",p);
-	return 0;
+	//char *p="Hello World";
+	//*p++;
+	//printf("%s\n",p);
+	//p++;
+	//printf("%s\n",p);
+	//return 0;
+	struct student{
+		char score;
+		int grade;
+		char num;
+		long long sex;
+	};
+	student *a=new student;
+	(*a).score=100;
+	a->grade=6;
+	a->num=10;
+	a->sex=1;
 }
 
